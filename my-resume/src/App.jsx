879c0e8c9a57eed0d7143cddd215822bd8b80a70
PALAK <palaktripathi789@gmail.com>
2026-08-20 11:20:52 +0530

    demo

--- my-resume/src/App.jsx
+++ my-resume/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import demo from './components/demo'
 
 function App() {
   
 
   return (
-    <h1>
-      Palak tripathi
-    </h1>
+    <>
+    <demo />
+    </>
+    
   )
 }
 
---
+++ my-resume/src/components/demo.jsx
@@ -0,0 +1,15 @@
+function demo(){
+    return(
+        <>
+        <h1>
+            Palak Tripathi
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Explicabo, dolore?
+
+        </p>
+        
+        </>
+    )
+}
+export default demo;
